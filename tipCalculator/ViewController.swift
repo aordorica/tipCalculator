045c6673b5c134d3527149e672e7ac00c64fb37e
Alan Ordorica <AlanOrdorica@sfs-wifi-dhcp-10-143-187-134.wifi.sfsu.edu>
2018-11-28 09:34:57 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Alan Ordorica on 11/28/18.
+//  Copyright © 2018 Alan Ordorica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
